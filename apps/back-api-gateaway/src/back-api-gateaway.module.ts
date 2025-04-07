4b4d3c1bd1f17153428de70cbd63627a15ae1ea2
lee <lollee31@yandex.ru>
2025-04-08 00:35:48 +0300

    CORE-3 refactroing cleints-modules and add filter modules

--- apps/back-api-gateaway/src/back-api-gateaway.module.ts
+++ apps/back-api-gateaway/src/back-api-gateaway.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { BackApiGateawayController } from './back-api-gateaway.controller';
 import { BackApiGateawayService } from './back-api-gateaway.service';
-import { UserModule } from './user/user.module';
-import { NichesModule } from './niches/niches.module';
+import { UserModule } from '../user-client/userClient.module';
+import { NichesModule } from '../niches-client/niches.module';
 
 @Module({
     imports: [UserModule, NichesModule],
--- apps/back-api-gateaway/user-client/userClient.service.ts
+++ apps/back-api-gateaway/user-client/userClient.service.ts
@@ -3,16 +3,14 @@ import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, throwError } from 'rxjs';
 
 @Injectable()
-export class UserService {
+export class UserClientService {
     constructor(@Inject('USERS_CLIENT') private usersClient: ClientProxy) {}
 
     findAll() {
         return this.usersClient.send('user.findAll', {}).pipe(
             catchError((error) => {
-                // Log the error
                 console.error('Error fetching users:', error);
 
-                // Throw a new error with a more user-friendly message
                 return throwError(() => new Error('Failed to fetch users. Please try again later.'));
             }),
         );
